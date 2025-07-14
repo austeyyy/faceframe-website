50f1a9c317465712547e6b17dc5b0328cdd5cafa
austeyyy <austeygfx@gmail.com>
2025-07-13 21:39:12 -0400

    analytics added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ScrollObserver from '../components/ScrollObserver'
+import { Analytics } from "@vercel/analytics/next"
 
 
 const inter = Inter({
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       >
         <ScrollObserver/>
         {children}
+        <Analytics />
       </body>
     </html>
   );
