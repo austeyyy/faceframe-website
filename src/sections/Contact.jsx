10a484455a268cd69e15acc3df50cab0de461d19
austeyyy <austeygfx@gmail.com>
2025-07-31 00:25:38 -0400

    contact button size fixed

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,7 +2,7 @@
 
 const Contact = () => {
   return (
-    <section id="contact-section" className="bg-[url('/images/image2.jpg')] bg-cover bg-center bg-no-repeat h-[425px] lg:h-[625px] scroll-mt-8">
+    <section id="contact-section" className="bg-[url('/images/image2.jpg')] bg-cover bg-center bg-no-repeat h-[450px] lg:h-[625px] scroll-mt-8">
         <div className="container h-full flex items-center py-24 md:py-40 ">
           <div className="text-center md:text-left align-center md:max-w-3xl">
             <h4 className="reveal font-semibold text-white text-base lg:text-lg mb-4" >LETS WORK</h4>
@@ -16,14 +16,14 @@ const Contact = () => {
                   
               </a>
               <a href="https://forms.gle/PBSMDFRXbC4nV4Y96">
-                  <button className="cursor-pointer bg-white border-[1px] border-gray-400 text-lg h-[52px] md:w-auto px-12 rounded-[17px] font-semibold inline-flex items-center justify-center gap-2 hover:bg-neutral-100 hover:scale-105 transition-all duration-300">
+                  <button className="cursor-pointer bg-white border-[1px] border-gray-400 text-lg h-[52px] md:w-auto px-8 md:px-12 rounded-[17px] font-semibold inline-flex items-center justify-center gap-2 transition-all duration-300">
                       Customers
                   </button>
                   
               </a>
 
               <a href="https://forms.gle/UNRR7gtojpQo4X2B8">
-                  <button className="cursor-pointer bg-white border-[1px] border-gray-400 text-lg h-[52px] md:w-auto px-12 rounded-[17px] font-semibold inline-flex items-center justify-center gap-2 hover:bg-neutral-100 hover:scale-105 transition-all duration-300">
+                  <button className="cursor-pointer bg-white border-[1px] border-gray-400 text-lg h-[52px] md:w-auto px-8 md:px-12 rounded-[17px] font-semibold inline-flex items-center justify-center gap-2 transition-all duration-300">
                       Businesses
                   </button>
                   
